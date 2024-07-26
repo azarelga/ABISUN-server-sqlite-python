2919e2fe1a17f01a33822aa84634dac4606f2587
azarelga <skootydack@gmail.com>
2024-07-26 22:54:00 +0700

    feat: more colors, html match

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import numpy as np
 import plotly.express as px
 import pandas as pd
 import requests
+from statistics import mode
 import joblib
 import time
 from keras.models import load_model
@@ -22,6 +23,11 @@ class App:
         self.start_time = time.strftime("%Y-%m-%d %H:%M:%S")
         self.subject_session = "Dewasa"
         self.labels = []
+        self.text_colors = ['green','red', 'red', 'red','green']
+        st.set_page_config(
+            page_title="ABISUN App",
+            page_icon="chart_with_upwards_trend",
+        )
         self.string_feedbacks = [
             "Kerja bagus! Kompresimu sudah stabil, mari pertahankan hingga akhir sesi!",
             "Tekananmu tidak stabil, kadang terlalu dangkal, kadang terlalu dalam, coba perdekat tekanan di sekitar 4 cm!",
@@ -38,59 +44,74 @@ class App:
         self.server_url = f"http://abisun-server.local:5000/last-request-time"  # Replace with your server URL
 
     def main(self):
-        if not st.session_state['running']:
-            # Halaman utama
-            st.title("Selamat Datang pada Simulasi RJP: ABISUN!")
-            st.header("Tata Cara CPR yang Benar")
-            st.write("""
-            1. Pastikan area aman untuk Anda dan subjek RJP.
-            2. Lakukan kompresi dada:
-                - Tempatkan tumit salah satu tangan di tengah dada korban, tepat di antara puting susu.
-                - Tempatkan tangan lainnya di atas tangan pertama dan kunci jari-jari Anda.
-                - Tekan dada korban ke bawah setidaknya 5 cm untuk orang dewasa, 4 cm untuk anak-anak, dan 3 cm untuk bayi.
-                - Lakukan kompresi dengan kecepatan 100-120 kali per menit.
-            3. Lanjutkan siklus 30 kompresi.
-            """)
-
-            st.header("Step by Step Simulasi")
-            st.write("""
-            0. Buatlah hotspot dari Handphone anda dengan detail sebagai berikut, pastikan laptop anda juga sudah terhubung:
-                - Nama Hotspot: ABISUN
-                - Password Hotspot: bantucpr
-            1. Pilih sesi: Dewasa, Anak, atau Bayi.
-            2. Mulai simulasi dengan menekan tombol di bawah.
-            3. Lakukan simulasi RJP pada maneken selama 60 detik.
-            4. Evaluasi performa RJP setelah simulasi telah selesai.
-            """)
-            self.start_button()
-            check_connection = st.empty()
-            while True:
-                with check_connection.container():
-                    self.check_connection()
-                    time.sleep(5)
+        if st.session_state['running'] == False:
+            self.home_page()
         else:
-            if self.subject_session == "Bayi":
-                self.model = load_model("../model/model_kedalaman_bayi.h5")
-            else: 
-                self.model = load_model("../model/model_kedalaman_dewasa_anak.h5")
-            self.labels=[]
-            countdown_placeholder = st.empty()
-            for i in range(5, 0, -1):
-                countdown_placeholder.text(f"Siap - siap! Simulasi CPR akan dimulai dalam {i} detik...")
-                time.sleep(1)
-            countdown_placeholder.empty()  # Clear countdown after starting
-            self.show_live()
-            self.result()
-            st.subheader("Mulai lagi?")
-            self.start_button()
+            self.run_simulation()
 
+    def home_page(self):
+        # Halaman utama
+        st.markdown("<h1 style='text-align: center;'>Selamat Datang pada Simulasi RJP: ABISUN!</h1>", unsafe_allow_html=True)
+        st.header("Tata Cara CPR yang Benar")
+        st.write("""
+        1. Pastikan area aman untuk Anda dan subjek RJP.
+        2. Lakukan kompresi dada:
+            - Tempatkan tumit salah satu tangan di tengah dada korban, tepat di antara puting susu.
+            - Tempatkan tangan lainnya di atas tangan pertama dan kunci jari-jari Anda.
+            - Tekan dada korban ke bawah setidaknya 5 cm untuk orang dewasa, 4 cm untuk anak-anak, dan 3 cm untuk bayi.
+            - Lakukan kompresi dengan kecepatan 100-120 kali per menit.
+        3. Lanjutkan siklus 30 kompresi.
+        """)
+
+        st.header("Step by Step Simulasi")
+        st.write("""
+        0. Buatlah hotspot dari Handphone anda dengan detail sebagai berikut, pastikan laptop anda juga sudah terhubung:
+            - Nama Hotspot: ABISUN
+            - Password Hotspot: bantucpr
+        1. Pilih sesi: Dewasa, Anak, atau Bayi.
+        2. Mulai simulasi dengan menekan tombol "Mulai Simulasi" di bawah.
+        3. Lakukan simulasi RJP pada maneken selama 60 detik.
+        4. Evaluasi performa RJP setelah simulasi telah selesai.
+        """)
+        self.start_button()
+        check_connection = st.empty()
+        while True:
+            with check_connection.container():
+                self.check_connection()
+                time.sleep(5)
+
+    def run_simulation(self):
+        # Load the appropriate model based on session type
+        if self.subject_session == "Bayi":
+            self.model = load_model("../model/model_kedalaman_bayi.h5")
+        else:
+            self.model = load_model("../model/model_kedalaman_dewasa_anak.h5")
+
+        self.labels = []
+
+        # Countdown before starting the simulation
+        countdown_placeholder = st.empty()
+        for i in range(5, 0, -1):
+            countdown_placeholder.text(f"Siap - siap! Simulasi CPR akan dimulai dalam {i} detik...")
+            time.sleep(1)
+        countdown_placeholder.empty()  # Clear countdown after starting
+
+        self.show_live()  # Function to show live simulation data
+        self.result()  # Function to show results after simulation
+
+        st.subheader("Mulai lagi?")
+        self.start_button()
+
+    def set_page(self):
+        st.session_state['running'] = True
 
     def start_button(self):
         self.subject_session = st.selectbox("Pilih Sesi", ["Dewasa", "Anak", "Bayi"])
-        if st.button("Mulai Simulasi", type='primary'):
-            st.session_state['running'] = True
-            st.session_state['done'] = False
-            st.empty()
+        st.button("Mulai Simulasi", 
+                  type='primary',
+                  use_container_width=True, 
+                  on_click=self.set_page()
+                  )
 
     def check_connection(self):
         try:
@@ -122,8 +143,9 @@ class App:
         label_index = 4
         for i in range(60, -1 , -1):
             with text_placeholder.container():
-                st.write(f"**{self.string_feedbacks[label_index]}**")
-                st.write(f"Sisa {i} detik lagi!")
+                st.markdown(f"<h3 style='color:{self.text_colors[label_index]}';>{self.string_feedbacks[label_index]}</h3>",
+                            unsafe_allow_html=True)
+                st.subheader(f"Sisa {i} detik lagi!")
                 print(self.labels)
                 print(label_index)
             self.time_series = read_df_60_seconds(self.start_time)
@@ -147,6 +169,25 @@ class App:
         fig.update_layout(xaxis_title='Waktu (detik)', 
                           yaxis_title='Kedalaman (cm)', 
                           modebar_remove=['select','zoom', 'pan'])
+        threshold = [[3.5,5.5],[2.5,4.5]]
+        if self.subject_session != "Bayi":
+            fig.add_hline(y=threshold[0][0],line_dash="dash", line_color="lightgreen")
+            fig.add_hline(y=threshold[0][1],line_dash="dash", line_color="lightgreen")
+            fig.add_hrect(
+                y0=threshold[0][0],
+                y1=threshold[0][1],
+                fillcolor="green", 
+                opacity=0.3
+            )
+        else:
+            fig.add_hline(y=threshold[1][0],line_dash="dash", line_color="lightgreen")
+            fig.add_hline(y=threshold[1][1],line_dash="dash", line_color="lightgreen")
+            fig.add_hrect(
+                y0=threshold[1][0],
+                y1=threshold[1][1],
+                fillcolor="green", 
+                opacity=0.3
+            )
         return fig
 
     def review_quality(self,df):
@@ -156,51 +197,51 @@ class App:
         y_pred_label = np.argmax(y_pred,axis=1)[0]
         return y_pred_label
 
-    # DEPRECATED
-    def split_dataframe(self, df):
-        # Calculate the size of each part
-        total_rows = 60
-        part_size = total_rows // 3
-
-        # Split the DataFrame into three parts based on the order
-        df_part1 = df.iloc[:part_size]
-        df_part2 = df.iloc[part_size:2*part_size]
-        df_part3 = df.iloc[2*part_size:]
-
-        def average_rows(df_part):
-            avg_df = pd.DataFrame((df_part['depth'].values[::2] + df_part['depth'].values[1::2]) / 2)
-            return avg_df
-
-        df_part1_avg = self.scaler.transform(average_rows(df_part1))
-        df_part2_avg = self.scaler.transform(average_rows(df_part2))
-        df_part3_avg = self.scaler.transform(average_rows(df_part3))
-
-        return df_part1_avg, df_part2_avg, df_part3_avg
-
     def result(self):
         result_placeholder = st.empty()
         with result_placeholder.container():
-            st.subheader("Berikut adalah hasil CPR anda!")
+            st.header("Berikut adalah hasil CPR anda!")
+            overall_quality = mode(self.labels)
+            st.markdown(
+                f"<h2>Kualitas RJP-mu secara garis besar: <span style='color : {self.text_colors[overall_quality]};'>{self.string_predictions[overall_quality]}</span></h2>", 
+                unsafe_allow_html=True)
             col1, col2, col3, col4, col5, col6= st.columns(6)
             with col1:
-                st.write(f"**Detik 0-10**")
-                st.write(self.string_predictions[self.labels[0]])
+                st.subheader(f"**Detik 0-10**")
+                st.markdown(
+                    f"<h3 style='color : {self.text_colors[self.labels[0]]}';>{self.string_predictions[self.labels[0]]}</h3>",
+                    unsafe_allow_html=True
+                )
             with col2:
-                st.write(f"**Detik 10-20**")
-                st.write(self.string_predictions[self.labels[1]])
+                st.subheader(f"**Detik 10-20**")
+                st.markdown(
+                    f"<h3 style='color : {self.text_colors[self.labels[1]]}';>{self.string_predictions[self.labels[1]]}</h3>",
+                    unsafe_allow_html=True
+                )
             with col3:
-                st.write(f"**Detik 20-30**")
-                st.write(self.string_predictions[self.labels[2]])
+                st.subheader(f"**Detik 20-30**")
+                st.markdown(
+                    f"<h3 style='color : {self.text_colors[self.labels[2]]}';>{self.string_predictions[self.labels[2]]}</h3>",
+                    unsafe_allow_html=True
+                )
             with col4:
-                st.write(f"**Detik 30-40**")
-                st.write(self.string_predictions[self.labels[3]])
+                st.subheader(f"**Detik 30-40**")
+                st.markdown(
+                    f"<h3 style='color : {self.text_colors[self.labels[3]]}';>{self.string_predictions[self.labels[3]]}</h3>",
+                    unsafe_allow_html=True
+                )
             with col5:
-                st.write(f"**Detik 40-50**")
-                st.write(self.string_predictions[self.labels[4]])
+                st.subheader(f"**Detik 40-50**")
+                st.markdown(
+                    f"<h3 style='color : {self.text_colors[self.labels[4]]}';>{self.string_predictions[self.labels[4]]}</h3>",
+                    unsafe_allow_html=True
+                )
             with col6:
-                st.write(f"**Detik 40-50**")
-                st.write(self.string_predictions[self.labels[5]])
-
+                st.subheader(f"**Detik 50-60**")
+                st.markdown(
+                    f"<h3 style='color : {self.text_colors[self.labels[5]]}';>{self.string_predictions[self.labels[5]]}</h3>",
+                    unsafe_allow_html=True
+                )
             chart_placeholder = st.empty()
             chart_placeholder.plotly_chart(self.chart_builder('depth'))
         if not st.session_state['done']:
